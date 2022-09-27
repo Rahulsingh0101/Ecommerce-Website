8e9331942f8459aa20e2e15d578e355372a3b448
Rahulsingh01 <rahulsinghbusiness0101@gmail.com>
2022-09-27 14:28:47 +0530

    Ecom all

--- Ecom/app/urls.py
+++ Ecom/app/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from app import views
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.ProductView.as_view(), name='home'),
-    path('product-detail/', views.product_detail, name='product-detail'),
+    path('product-detail/',views.ProductDetailView.as_view(), name='product-detail'),
     path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
-    path('cart/', views.add_to_cart, name='add-to-cart'),
+    path('add_to_cart/', views.add_to_cart, name='add-to-cart'),
+    path('cart/', views.show_cart, name='showcart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
     path('address/', views.address, name='address'),
--- Ecom/app/views.py
+++ Ecom/app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http.response import HttpResponse
 from django.views import View
-from app.models import Product,Cart
+from app.models import Product,Cart,Customer
 from django.db.models import Q
 
 
@@ -27,16 +27,34 @@ class ProductDetailView(View):
             item_already_in_cart = Cart.objects.filter(Q(product=product.id) & Q(user=request.user)).exists()
             return render(request, 'app/productdetail.html',{'product':product, 'item_already_in_cart':item_already_in_cart})
 
-
-
-def home(request):
- return render(request, 'app/home.html')
-
-def product_detail(request):
- return render(request, 'app/productdetail.html')
-
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+    try:
+        user = request.user
+        product_id =request.GET.get('prod_id')
+        product = Product.objects.get(id=product_id)
+        Cart(user=user, product=product).save()
+    except:   
+        return redirect('/cart')
+
+
+def show_cart(request):
+    if request.user.is_authenticated:
+        user=request.user
+        cart= Cart.objects.filter(user=user)
+        amount = 0.0
+        shipping_amount=70.0
+        total_amount=0.0
+        cart_product=[p for p in Cart.objects.all() if p.user == user]
+        print(cart_product)
+        if cart_product:
+            for p in cart_product:
+                tempamount=(p.quantity*p.product.discounted_price)
+                amount+= tempamount
+                total_amount=amount+shipping_amount
+            return render(request, 'app/addtocart.html',{'carts':cart, 'total_amount':total_amount, 'amount':amount})
+        else:
+            return render(request, 'app/emptycart.html')
+ 
 
 def buy_now(request):
  return render(request, 'app/buynow.html')
