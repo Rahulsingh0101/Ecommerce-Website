ef28ded0a825d539bbbb5024cb24f5f077f0fa69
Rahulsingh01 <rahulsinghbusiness0101@gmail.com>
2022-09-29 13:50:46 +0530

    Ecomm

--- Ecom/app/views.py
+++ Ecom/app/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render,redirect
-from django.http.response import HttpResponse
+from itertools import product
+# from statistics import quantiles
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
 from django.views import View
-from app.models import Product,Cart,Customer
+from .models import Customer, Product, Cart, OrderPlaced
+from django.contrib import messages
 from django.db.models import Q
-
+from django.http import JsonResponse
+# from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.models import User
 
 
 # def home(request):
@@ -33,7 +39,7 @@ def add_to_cart(request):
         product_id =request.GET.get('prod_id')
         product = Product.objects.get(id=product_id)
         Cart(user=user, product=product).save()
-    except:   
+    except:    
         return redirect('/cart')
 
 
@@ -45,7 +51,7 @@ def show_cart(request):
         shipping_amount=70.0
         total_amount=0.0
         cart_product=[p for p in Cart.objects.all() if p.user == user]
-        print(cart_product)
+        # print(cart_product)
         if cart_product:
             for p in cart_product:
                 tempamount=(p.quantity*p.product.discounted_price)
@@ -54,7 +60,119 @@ def show_cart(request):
             return render(request, 'app/addtocart.html',{'carts':cart, 'total_amount':total_amount, 'amount':amount})
         else:
             return render(request, 'app/emptycart.html')
+
+
+def plus_cart(request):
+    if request.method == 'GET':
+        prod_id= request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity+=1
+        c.save()
+        amount =0.0
+        shipping_amount=70.0
+        cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+        for p in cart_product:
+            tempamount=(p.quantity * p.product.discounted_price)
+            amount+= tempamount
+            
+
+        data ={
+            'quantity': c.quantity,
+            'amount': amount,
+            'totalamount': amount + shipping_amount
+            }
+        return JsonResponse(data)    
+
+
+def minus_cart(request):
+    if request.method == 'GET':
+        prod_id= request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity-=1
+        c.save()
+        amount =0.0
+        shipping_amount=70.0
+        cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+        for p in cart_product:
+            tempamount=(p.quantity * p.product.discounted_price)
+            amount+= tempamount
+            
+
+        data ={
+            'quantity': c.quantity,
+            'amount': amount,
+            'totalamount': amount + shipping_amount
+            }
+        return JsonResponse(data)  
+
+
+def remove_cart(request):
+    if request.method == 'GET':
+        prod_id= request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.delete()
+        amount =0.0
+        shipping_amount=70.0
+        cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+        for p in cart_product:
+            tempamount=(p.quantity * p.product.discounted_price)
+            amount+= tempamount
+
+        data ={
+             'amount': amount,
+            'totalamount': amount + shipping_amount
+            }
+        return JsonResponse(data)           
+
+
+def mobile(request, data=None):
+    if data == None:
+        mobiles = Product.objects.filter(category='M')
+    elif data=='Redmi' or data== 'Samsung':
+        mobiles = Product.objects.filter(category='M').filter(brand=data)
+    elif data == 'below':
+        mobiles = Product.objects.filter(category='M').filter(discounted_price__lt=10000)
+    elif data == 'above':
+        mobiles = Product.objects.filter(category='M').filter(discounted_price__gt=10000)
+    return render(request, 'app/mobile.html',{'mobiles':mobiles})     
+
+
+def laptop(request, data=None):
+    if data == None:
+        laptops = Product.objects.filter(category='L')
+    elif data=='Asus' or data== 'Dell':
+        laptops = Product.objects.filter(category='L').filter(brand=data)
+    elif data == 'below':
+        laptops = Product.objects.filter(category='L').filter(discounted_price__lt=25000)
+    elif data == 'above':
+        laptops = Product.objects.filter(category='L').filter(discounted_price__gt=25000)
+    return render(request, 'app/laptop.html',{'laptops':laptops})                  
  
+
+def topwear(request, data=None):
+    if data == None:
+        topwears = Product.objects.filter(category='TW')
+    elif data=='ZARA' or data== 'GUCCI':
+        topwears = Product.objects.filter(category='TW').filter(brand=data)
+    elif data == 'below':
+        topwears = Product.objects.filter(category='TW').filter(discounted_price__lt=1000)
+    elif data == 'above':
+        topwears = Product.objects.filter(category='TW').filter(discounted_price__gt=1000)
+    return render(request, 'app/topwear.html',{'topwears':topwears})
+
+
+def bottomwear(request, data=None):
+    if data == None:
+        bottomwears = Product.objects.filter(category='BW')
+    elif data=='ZARA' or data== 'GUCCI':
+        bottomwears = Product.objects.filter(category='BW').filter(brand=data)
+    elif data == 'below':
+        bottomwears = Product.objects.filter(category='BW').filter(discounted_price__lt=1000)
+    elif data == 'above':
+        bottomwears = Product.objects.filter(category='BW').filter(discounted_price__gt=1000)
+    return render(request, 'app/bottomwear.html',{'bottomwears':bottomwears})
+
+
 
 def buy_now(request):
  return render(request, 'app/buynow.html')
